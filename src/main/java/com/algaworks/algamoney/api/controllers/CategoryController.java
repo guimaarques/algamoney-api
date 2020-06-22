3d285ce543bca1082b1759c351b21c3cf450957d
Guilherme Marques <guih_marques@hotmail.com>
2020-06-21 22:05:09 -0300

    adding get by code and post method

--- src/main/java/com/algaworks/algamoney/api/controllers/CategoryController.java
+++ src/main/java/com/algaworks/algamoney/api/controllers/CategoryController.java
@@ -3,11 +3,14 @@ package com.algaworks.algamoney.api.controllers;
 import com.algaworks.algamoney.api.models.Category;
 import com.algaworks.algamoney.api.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categories")
@@ -19,5 +22,21 @@ public class CategoryController {
     @GetMapping
     public List<Category> categoryList(){
         return categoryRepository.findAll();
+    }
+
+    @GetMapping("/{code}")
+    public Optional<Category> categoryByCode(@PathVariable Long code){
+        return categoryRepository.findById(code);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> create(@RequestBody Category category,
+                                 HttpServletResponse request){
+        Category newCategory = categoryRepository.save(category);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{code}")
+                    .buildAndExpand(newCategory.getCategoryCode()).toUri();
+
+        request.setHeader("Location", uri.toASCIIString());
+        return ResponseEntity.created(uri).body(newCategory);
     }
 }
